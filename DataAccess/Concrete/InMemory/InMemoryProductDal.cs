bb9415152081f2848c8284222c37169980c3a3b6
Salih <Salih@DESKTOP-Q8NRVSB>
2021-03-02 01:14:58 +0300

    .

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -8,9 +8,25 @@ namespace DataAccess.Concrete.InMemory
 {
     public class InMemoryProductDal : IProductDal
     {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+                new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1}
+
+            };
+        }
+
+
+
         public void Add(Product product)
         {
-            throw new NotImplementedException();
+            _products.Add(product);
         }
 
         public void Delete(Product product)
@@ -20,7 +36,7 @@ namespace DataAccess.Concrete.InMemory
 
         public List<Product> GetAll()
         {
-            throw new NotImplementedException();
+            return _products
         }
 
         public void Update(Product product)
